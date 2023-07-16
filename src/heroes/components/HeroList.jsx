592213980f0e3a3327f0a2cf0d04ecd99b686ce5
Cristianps1988 <cristianps_1@hotmail.com>
2023-07-16 13:34:02 -0500

    bug fixed

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -5,7 +5,7 @@ import { HeroCard } from './'
 export const HeroList = ({ publisher }) => {
     const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
     return (
-        <div className='row row-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn'>
+        <div className='row row-cols-1 row-cols-md-3 g-3'>
             {heroes.map(heroe => <HeroCard key={heroe.id} {...heroe} />)}
         </div>
     )
